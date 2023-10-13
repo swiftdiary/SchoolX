8c1af8c77207d64c811d8fa318fefba5991eacff
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2023-10-13 16:10:54 +0500

    first commit to the project

---
+++ SchoolX/Auth/AuthScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AuthScreen.swift
+//  SchoolX
+//
+//  Created by Akbar Khusanbaev on 13/10/23.
+//
+
+import SwiftUI
+
+struct AuthScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthScreen()
+}
