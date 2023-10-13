d0eb795c512c243a6cc2bf0c7a8126ed6695cc43
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2023-10-13 14:34:30 +0500

    Initial Commit

---
+++ SchoolX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchoolX
+//
+//  Created by Akbar Khusanbaev on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchoolX/SchoolXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolXApp.swift
+//  SchoolX
+//
+//  Created by Akbar Khusanbaev on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SchoolXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
