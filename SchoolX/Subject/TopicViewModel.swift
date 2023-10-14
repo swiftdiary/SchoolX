326f83630fe0c18ca7afe8051363200a4675f80c
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2023-10-14 17:01:11 +0500

    added MainActor

--- SchoolX/Subject/TopicViewModel.swift
+++ SchoolX/Subject/TopicViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 final class TopicViewModel: ObservableObject {
     @Published var topics: [Topic] = []
     
+    @MainActor
     func getTopics(with subject: SubjectType) async throws {
         topics = try await TopicManager.shared.getTopicsFiltered(by: subject)
     }
