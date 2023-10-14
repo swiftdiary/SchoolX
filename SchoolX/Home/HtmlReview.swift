65f8e13b77e2606b720408973daea3fb108a26c2
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2023-10-14 11:14:51 +0500

    Password checker added

---
+++ SchoolX/Home/HtmlReview.swift
@@ -0,0 +1,18 @@
+//
+//  HtmlReview.swift
+//  SchoolX
+//
+//  Created by Muhammadjon Madaminov on 14/10/23.
+//
+
+import SwiftUI
+
+struct HtmlReview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HtmlReview()
+}
