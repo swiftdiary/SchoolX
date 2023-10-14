7670e299b77277a0f974242a9b7af38f81b52858
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2023-10-14 18:02:57 +0500

    fixed animations

---
+++ SchoolX/Progress/Progress.swift
@@ -0,0 +1,12 @@
+//
+//  Progress.swift
+//  SchoolX
+//
+//  Created by Akbar Khusanbaev on 14/10/23.
+//
+
+import Foundation
+
+struct Progress {
+    
+}
--- SchoolX/SplashScreen.swift
+++ SchoolX/SplashScreen.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SplashScreen: View {
     var body: some View {
         VStack {
-            LottieView()
+            LottieView(name: "loading")
         }
         .padding()
     }
