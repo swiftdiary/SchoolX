3dd8ef1e572bbf046e8c750a72d1720af77d3e3a
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2023-10-14 10:30:58 +0500

    Commited

--- SchoolX/Auth/AuthScreen.swift
+++ SchoolX/Auth/AuthScreen.swift
@@ -15,51 +15,70 @@ struct AuthScreen: View {
     @EnvironmentObject private var appNavigation: AppNavigation
     var body: some View {
         VStack {
-            Text("Welcome!")
-                .font(.title.bold())
-            Button(action: {
-                Task {
-                    do {
-                        try await viewModel.signInApple()
-                        
-                    } catch {
-                        print(error)
+            
+            Image("Logo")
+                .resizable()
+                .frame(width: 200, height: 220)
+                .padding(.bottom, 30)
+            
+            VStack {
+                
+                
+                Text("Welcome!")
+                    .font(.title.bold())
+                    .fontDesign(.rounded)
+                Button(action: {
+                    Task {
+                        do {
+                            try await viewModel.signInApple()
+                            
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }, label: {
+                    SignInWithAppleButtonViewRepresentable(type: .default, style: .black)
+                        .allowsHitTesting(false)
+                })
+                .frame(height: 55)
+                Divider()
+                    .padding()
+                GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .dark, style: .wide, state: .normal)) {
+                    Task {
+                        do {
+                            try await viewModel.signInGoogle()
+                            
+                        } catch {
+                            print(error)
+                        }
                     }
                 }
-            }, label: {
-                SignInWithAppleButtonViewRepresentable(type: .default, style: .black)
-                    .allowsHitTesting(false)
-            })
-            .frame(height: 55)
-            Divider()
-                .padding()
-            GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .dark, style: .wide, state: .normal)) {
-                Task {
-                    do {
-                        try await viewModel.signInGoogle()
-                        
-                    } catch {
-                        print(error)
-                    }
+                .frame(height: 55)
+                Divider()
+                    .padding()
+                Button {
+                    appNavigation.path.append(.signInWithEmail)
+                } label: {
+                    Text("Continue using Email")
+                        .foregroundStyle(Color.white)
                 }
+                .frame(maxWidth: .infinity)
+                .frame(height: 55)
+                .background(.accent)
+                .font(.headline)
+                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                
             }
-            .frame(height: 55)
-            Divider()
-                .padding()
-            Button {
-                appNavigation.path.append(.signInWithEmail)
-            } label: {
-                Text("Continue using Email")
-            }
-            .frame(maxWidth: .infinity)
-            .frame(height: 55)
-            .background(Color.gray.gradient.opacity(0.3))
-            .font(.headline)
-            .clipShape(RoundedRectangle(cornerRadius: 10.0))
-            .foregroundStyle(.accent)
+            .padding(9)
+            .padding(.vertical)
+            .toolbar(.hidden, for: .navigationBar)
+            .padding(.vertical)
+            .background(
+                Color.accentColor.opacity(0.15)
+            )
+            .cornerRadius(15)
+            .padding(8)
         }
-        .padding()
-        .toolbar(.hidden, for: .navigationBar)
     }
 }
 
